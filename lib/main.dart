acfb8f421c1427d03bc0af25424bd151d88e2c5b
Tarun <tarunchichili@gmail.com>
2019-04-14 22:47:01 -0700

    add persistant bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,175 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final Key keyOne = PageStorageKey('pageOne');
+  final Key keyTwo = PageStorageKey('pageTwo');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int currentTab = 0;
+
+  PageOne one;
+  PageTwo two;
+  List<Widget> pages;
+  Widget currentPage;
+
+  List<Data> dataList;
+  final PageStorageBucket bucket = PageStorageBucket();
+
+  @override
+  void initState() {
+    dataList = [
+      Data(1, false, "Example-1"),
+      Data(2, false, "Example-2"),
+      Data(3, false, "Example-3"),
+      Data(4, false, "Example-4"),
+      Data(5, false, "Example-5"),
+      Data(6, false, "Example-6"),
+      Data(7, false, "Example-7"),
+      Data(8, false, "Example-8"),
+      Data(9, false, "Example-9"),
+      Data(10, false, "Example-10"),
+    ];
+    one = PageOne(
+      key: keyOne,
+      dataList: dataList,
+    );
+    two = PageTwo(
+      key: keyTwo,
+    );
+
+    pages = [one, two];
+
+    currentPage = one;
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Persistance Example"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: PageStorage(
+        child: currentPage,
+        bucket: bucket,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentTab,
+        onTap: (int index) {
+          setState(() {
+            currentTab = index;
+            currentPage = pages[index];
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("Settings"),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class PageOne extends StatefulWidget {
+  final List<Data> dataList;
+  PageOne({
+    Key key,
+    this.dataList,
+  }) : super(key: key);
+
+  @override
+  PageOneState createState() => PageOneState();
+}
+
+class PageOneState extends State<PageOne> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.dataList.length,
+        itemBuilder: (context, index) {
+          return ExpansionTile(
+            key: PageStorageKey('${widget.dataList[index].id}'),
+            title: Text(widget.dataList[index].title),
+            // onExpansionChanged: (b) => setState(() {
+            //       widget.dataList[index].expanded = b;
+            //       PageStorage.of(context).writeState(context, b,
+            //           identifier: ValueKey(
+            //             '${widget.dataList[index].id}',
+            //           ));
+            //     }),
+            // initiallyExpanded: widget.dataList[index].expanded,
+            //  PageStorage.of(context).readState(
+            //           context,
+            //           identifier: ValueKey(
+            //             '${widget.dataList[index].id}',
+            //           ),
+            //         ) ??
+            //     false,
+            children: <Widget>[
+              Container(
+                color: index % 2 == 0 ? Colors.orange : Colors.limeAccent,
+                height: 100.0,
+              ),
+            ],
+          );
+        });
+  }
+}
+
+class PageTwo extends StatefulWidget {
+  PageTwo({Key key}) : super(key: key);
+
+  @override
+  PageTwoState createState() => PageTwoState();
+}
+
+class PageTwoState extends State<PageTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 250.0,
+      itemBuilder: (context, index) => Container(
+        padding: EdgeInsets.all(10.0),
+        child: Material(
+          elevation: 4.0,
+          borderRadius: BorderRadius.circular(5.0),
+          color: index % 2 == 0 ? Colors.cyan : Colors.deepOrange,
+          child: Center(
+            child: Text(index.toString()),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Data {
+  final int id;
+  bool expanded;
+  final String title;
+  Data(this.id, this.expanded, this.title);
 }
